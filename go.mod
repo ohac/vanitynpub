da2819a9c10cd86fdb738099a64b361ff48ff484
OHASHI Hideya <ohachige@gmail.com>
2025-08-09 11:17:48 +0900

    first commit

---
+++ go.mod
@@ -0,0 +1,41 @@
+module github.com/ohac/vanitynpub
+
+go 1.24.1
+
+toolchain go1.24.6
+
+require (
+	github.com/FactomProject/btcutilecc v0.0.0-20130527213604-d3a63a5752ec
+	github.com/btcsuite/btcd/btcec/v2 v2.3.5
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.4.0
+	github.com/nbd-wtf/go-nostr v0.51.12
+	github.com/tyler-smith/go-bip32 v1.0.0
+	github.com/tyler-smith/go-bip39 v1.1.0
+)
+
+require (
+	github.com/FactomProject/basen v0.0.0-20150613233007-fe3947df716e // indirect
+	github.com/ImVexed/fasturl v0.0.0-20230304231329-4e41488060f3 // indirect
+	github.com/btcsuite/btcd/btcutil v1.1.5 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.1.0 // indirect
+	github.com/bytedance/sonic v1.13.1 // indirect
+	github.com/bytedance/sonic/loader v0.2.4 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/coder/websocket v1.8.12 // indirect
+	github.com/decred/dcrd/crypto/blake256 v1.1.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/mailru/easyjson v0.9.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/puzpuzpuz/xsync/v3 v3.5.1 // indirect
+	github.com/tidwall/gjson v1.18.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	golang.org/x/arch v0.15.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+)
---
+++ nip06.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha512"
+	"encoding/hex"
+	"fmt"
+	"hash"
+	"math/big"
+	"sync"
+
+	_ "github.com/nbd-wtf/go-nostr"
+	"github.com/nbd-wtf/go-nostr/nip06"
+	"github.com/nbd-wtf/go-nostr/nip19"
+	"github.com/tyler-smith/go-bip32"
+	"github.com/tyler-smith/go-bip39"
+
+	btcutil "github.com/FactomProject/btcutilecc"
+	_ "github.com/btcsuite/btcd/btcec/v2"
+	_ "github.com/btcsuite/btcd/btcec/v2/schnorr"
+	secp "github.com/decred/dcrd/dcrec/secp256k1/v4"
+)
+
+var (
+	curve = btcutil.Secp256k1()
+)
+
+func GenerateSeedWords() (string, error) {
+	entropy, err := bip39.NewEntropy(256)
+	if err != nil {
+		return "", err
+	}
+
+	words, err := bip39.NewMnemonic(entropy)
+	if err != nil {
+		return "", err
+	}
+
+	return words, nil
+}
+
+func SeedFromWords(words string) []byte {
+	return bip39.NewSeed(words, "")
+}
+
+func compressPublicKey(x *big.Int, y *big.Int) *bytes.Buffer {
+	var key bytes.Buffer
+	key.WriteByte(byte(0x2) + byte(y.Bit(0)))
+	xBytes := x.Bytes()
+	for i := 0; i < (bip32.PublicKeyCompressedLength - 1 - len(xBytes)); i++ {
+		key.WriteByte(0x0)
+	}
+	key.Write(xBytes)
+	return &key
+}
+
+func keyToBytes(key []byte) []byte {
+	var data []byte
+	x, y := curve.ScalarBaseMult(key)
+	bb := compressPublicKey(x, y)
+	childIndexBytes := make([]byte, 4)
+	bb.Write(childIndexBytes)
+	data = bb.Bytes()
+	return data
+}
+
+func keyToBigInt(key []byte) big.Int {
+	var key2Int big.Int
+	key2Int.SetBytes(key)
+	return key2Int
+}
+
+func newChildKey(data []byte, key1Int, key2Int *big.Int,
+	key *bip32.Key, childIdx uint32, hmac hash.Hash, n *big.Int) []byte {
+	data[33] = byte(childIdx >> 24)
+	data[34] = byte(childIdx >> 16)
+	data[35] = byte(childIdx >> 8)
+	data[36] = byte(childIdx)
+	hmac.Write(data)
+	intermediary := hmac.Sum(nil)
+	var key1 []byte
+	key1 = intermediary[:32]
+	key1Int.SetBytes(key1)
+	key1Int.Add(key1Int, key2Int)
+	key1Int.Mod(key1Int, n)
+	b := key1Int.Bytes()
+	return b
+}
+
+func mining(seed []byte, thread uint32, lenckeymax int, filter1, filter2 byte) error {
+	key, err := bip32.NewMasterKey(seed)
+	if err != nil {
+		return err
+	}
+
+	derivationPath := []uint32{
+		bip32.FirstHardenedChild + 44,
+		bip32.FirstHardenedChild + 1237,
+		bip32.FirstHardenedChild + 0,
+		thread,
+	}
+
+	next := key
+	for _, idx := range derivationPath {
+		var err error
+		if next, err = next.NewChildKey(idx); err != nil {
+			return err
+		}
+	}
+
+	data := keyToBytes(next.Key)
+	var key1Int big.Int
+	key2Int := keyToBigInt(next.Key)
+	var chainCode []byte
+	chainCode = next.ChainCode
+	hmac := hmac.New(sha512.New, chainCode)
+	max := uint32(4)
+	max = bip32.FirstHardenedChild
+	n := curve.Params().N
+	for i := uint32(0); i < max; i++ {
+		var ckey []byte
+		hmac.Reset()
+		ckey = newChildKey(data, &key1Int, &key2Int, next, i, hmac, n)
+		lenckey := len(ckey)
+		if lenckey < lenckeymax {
+			continue
+		}
+		privKey := secp.PrivKeyFromBytes(ckey)
+		pk := privKey.PubKey()
+		schn := pk.SerializeCompressed()
+		schn = schn[1:]
+
+		if schn[0] == filter1 && schn[1] == filter2 {
+			npub := hex.EncodeToString(schn)
+			npubs, _ := nip19.EncodePublicKey(npub)
+			if npubs[:9] == "npub10hac" {
+				fmt.Println(thread, i, npubs, npub[:4], schn[:4], lenckey)
+			}
+		}
+		if i&0x3fffff == 0 && thread == 0 {
+			fmt.Println(thread, i)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	words, _ := nip06.GenerateSeedWords()
+
+	// test vectors
+	words = "leader monkey parrot ring guide accident before fence cannon height naive bean"
+
+	fmt.Println(words)
+	seed := nip06.SeedFromWords(words)
+	offset := uint32(7)
+
+	// npub10hac
+	lenckeymax := 32 // TODO or 30 or 31
+	filter1 := byte(125)
+	filter2 := byte(251)
+
+	cores := 6
+
+	var wg sync.WaitGroup
+	wg.Add(cores)
+	for i := 0; i < cores; i++ {
+		go mining(seed, offset+1+uint32(i), lenckeymax, filter1, filter2)
+	}
+	wg.Wait()
+}
